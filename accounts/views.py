24dd87a841e422685b9816770c1feb4d8378fcaf
KempeR1t <kemper1t@mail.ru>
2018-11-02 14:58:08 +0300

    done

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import AccountUserForm
+
+
+def account_login(request):
+    form = AccountUserForm()
+
+    if request.method=='POST':
+        print('*'*50)
+        print(request.POST)
+        print('*' * 50)
+
+    return render(request, 'accounts/login.html', {'form': form})
